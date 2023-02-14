0271ca64d65900c8cecc9297e5e4df80e072f4d7
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-02-14 10:43:28 -0300

    API route example

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// To access these API routes, you must prefixe them with '/api'
+// Example: http://127.0.0.1:8000/api/posts
+
+Route::get('/posts', function() {
+    return response()->json([
+        'posts' => [
+            [
+                'title' => 'Post One'
+            ]
+        ]
+    ]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
