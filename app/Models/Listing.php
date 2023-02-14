ff0331fbd59c1b4127fceba47586ae5f95636023
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-02-14 18:36:08 -0300

    Migration and Seeder for Listing table

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,46 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-// class Listing extends Model
-class Listing
+class Listing extends Model
 {
-    // use HasFactory;
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'discription' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
-                    printer took a galley of type and scrambled it to make a type specimen book. It has survived 
-                    not only five centuries, but also the leap into electronic typesetting, remaining essentially 
-                    unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem 
-                    Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-                    including versions of Lorem Ipsum."
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'discription' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
-                    printer took a galley of type and scrambled it to make a type specimen book. It has survived 
-                    not only five centuries, but also the leap into electronic typesetting, remaining essentially 
-                    unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem 
-                    Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-                    including versions of Lorem Ipsum."
-            ],
-        ];
-    }
-
-    public static function find($id) { // Finding an id, without using Eloquent ORM
-        $listings = self::all();
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,6 @@
 <?php
-
 namespace Database\Seeders;
+use App\Models\Listing;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,5 +20,28 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+        // php artisan db:seed 
+        /* Will create these two registrations on the Listing table in the DB */
+        Listing::create([
+            'title' => 'Laravel Senior Develop',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'email1@email.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus purus 
+            sollicitudin diam ultricies vulputate. Sed et odio volutpat magna tincidunt dictum eget nec enim.',
+        ]);
+        Listing::create([
+            'title' => 'Full-Stack Engineer',
+            'tags' => 'laravel, backend, api',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'email1@email.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus purus 
+            sollicitudin diam ultricies vulputate. Sed et odio volutpat magna tincidunt dictum eget nec enim.',
+        ]);
     }
 }
