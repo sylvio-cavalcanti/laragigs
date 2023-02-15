87c02cc030eec6ab670802cbebe869fc967f1a48
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-02-15 11:17:12 -0300

    Adding the Theme HTML and Template Partials for _hero and _search

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,7 +1,8 @@
 @extends('layout')
 @section('content')
-    {{-- must match the name set in @yield --}}
-    <h1>Listing: </h1>
-    <h2>{{ $listing['title'] }}</h2>
-    <p>{{ $listing['description'] }}</p>
+@include('partials._search')
+{{-- must match the name set in @yield --}}
+<h1>Listing: </h1>
+<h2>{{ $listing['title'] }}</h2>
+<p>{{ $listing['description'] }}</p>
 @endsection
