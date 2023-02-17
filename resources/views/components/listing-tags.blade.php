ad478cdafdaafd92078f65f3145c130276524a1d
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-02-17 17:13:10 -0300

    Feat: Component for the listing tags. Anchor tag with query parameter

---
+++ resources/views/components/listing-tags.blade.php
@@ -0,0 +1,16 @@
+{{-- The tags are commas separated list coming from the DB --}}
+@props(['tagsCsv'])
+
+@php
+    /* Gets the comma separated string and turns it into an array  */
+    $tags = explode(',', $tagsCsv);
+@endphp
+
+<ul class="flex">
+    @foreach ($tags as $tag)
+        <li
+            class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+            <a href="/?={{$tag}}">{{ $tag }}</a> {{-- The href has a query parameter for the current tag --}}
+        </li>
+    @endforeach
+</ul>
